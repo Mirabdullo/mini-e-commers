4e99763b87d7429db773dc13f2c846566ba20f7b
Mahmudov Mirabdulloh <mmirabdullo1999@gmail.com>
2022-12-04 22:46:09 +0500

    Add sub-category service

--- src/sub-categories/sub-categories.controller.ts
+++ src/sub-categories/sub-categories.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { CreateSubCategoriesDto } from './dto/create-sub-categories.dto';
+import { SubCategory } from './sub-categories.model';
+import { SubCategoriesService } from './sub-categories.service';
 
 @Controller('sub-categories')
-export class SubCategoriesController {}
+export class SubCategoriesController {
+    constructor(private readonly subCategoryService: SubCategoriesService){}
+
+
+    @ApiOperation({summary: 'Sub categoriya qoshish'})
+    @ApiResponse({status: 201, type: SubCategory})
+    @Post()
+    create(@Body() createSubCategoriesDto: CreateSubCategoriesDto){
+        return this.subCategoryService.create(createSubCategoriesDto)
+    }
+
+
+    @ApiOperation({summary: "Barcha sub categoriyalar"})
+    @ApiResponse({status: 200, type: [SubCategory]})
+    @Get()
+    getAll(){
+        return this.subCategoryService.getAll()
+    }
+
+
+    @ApiOperation({summary: "Berilgan id bo'yicha sub categoriya"})
+    @ApiResponse({status: 200, type: SubCategory})
+    @Get(':id')
+    getOneById(@Param('id') id: number){
+        return this.subCategoryService.getOneById(id)
+    }
+
+
+
+
+
+
+}
--- src/sub-categories/sub-categories.service.ts
+++ src/sub-categories/sub-categories.service.ts
@@ -1,4 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateSubCategoriesDto } from './dto/create-sub-categories.dto';
+import { UpdateSubCategoriesDto } from './dto/update-sub-categories.dto';
+import { SubCategory } from './sub-categories.model';
 
 @Injectable()
-export class SubCategoriesService {}
+export class SubCategoriesService {
+  constructor(
+    @InjectModel(SubCategory) private subCategoryRepository: typeof SubCategory,
+  ) {}
+
+  async create(createSubCategoriesDto: CreateSubCategoriesDto) {
+    try {
+      const subCategory = await this.subCategoryRepository.create(
+        createSubCategoriesDto,
+      );
+      return subCategory;
+    } catch (error) {
+      throw new HttpException('Serverda xatolik!', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  async getAll() {
+    try {
+      const subCategorys = await this.subCategoryRepository.findAll({
+        include: { all: true },
+      });
+      return subCategorys;
+    } catch (error) {
+      throw new HttpException('Serverda xatolik!', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  async getOneById(id: number) {
+    try {
+      const subCategory = await this.subCategoryRepository.findByPk(id, {
+        include: { all: true },
+      });
+      if (!subCategory) {
+        throw new HttpException("Ma'lumot topilmadi", HttpStatus.NOT_FOUND);
+      }
+      return subCategory;
+    } catch (error) {
+      throw new HttpException('Serverda xatolik!', HttpStatus.FORBIDDEN);
+    }
+  }
+
+  async updateSubCategory(
+    id: number,
+    updateSubCategoryDto: UpdateSubCategoriesDto,
+  ) {
+    try {
+      const subCategory = await this.subCategoryRepository.findByPk(id);
+      if (!subCategory) {
+        throw new HttpException("Ma'lumot topilmadi", HttpStatus.NOT_FOUND);
+      }
+      const updateSubCategory = await this.subCategoryRepository.update(updateSubCategoryDto,{where: {id}, returning: true})
+      return updateSubCategory
+    } catch (error) {
+      throw new HttpException('Serverda xatolik!', HttpStatus.FORBIDDEN);
+    }
+  }
+
+
+  async deleteSubCategory(id: number){
+    try {
+        const subCategory = await this.subCategoryRepository.findByPk(id);
+        if (!subCategory) {
+          throw new HttpException("Ma'lumot topilmadi", HttpStatus.NOT_FOUND);
+        }
+        await this.subCategoryRepository.destroy({where: {id}})
+        return subCategory
+    } catch (error) {
+        throw new HttpException('Serverda xatolik!', HttpStatus.FORBIDDEN);
+    }
+  }
+
+
+}
